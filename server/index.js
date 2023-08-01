d35209e826a4433360dbc0b146c60c35ef066c2a
caesar <caesar.ladion@gmail.com>
2023-08-01 11:26:42 -0700

    added index.js

---
+++ server/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import boardRoutes from "./routes/boards.js";
+import listRoutes from "./routes/lists.js";
+import cardRoutes from "./routes/cards.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+//middlewares
+app.use(cookieParser())
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api/lists", listRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("/api/comments", commentRoutes);
+
+//error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to Server");
+});
